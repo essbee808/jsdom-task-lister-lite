6ad8232f467e7b40118d3f66e7221c5412a7b4cd
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-18 21:52:38 -0700

    commit tree 6ca91bce30dde6c7289db9d1261d0678b7066b73

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  document.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let str = document.getElementById('new-task-description').value;
+    document.getElementById('tasks').innerHTML = str
+  });
+
+  // function displayTask() {
+  //   let str = document.getElementById('new-task-description').value;
+  //   document.getElementById('tasks').innerHTML = str
+  // }
+  // document.addEventListener("click", function(e) {
+  //  let str = document.getElementById('new-task-description').value
+  //  document.getElementById('tasks').innerHTML = str 
+  // })
 });
+
+// Type a task into input field
+// Click some form of a submit button
+// My string should appear on the DOM after submit button was activated
